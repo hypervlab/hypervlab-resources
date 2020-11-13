6b6f382e729b64a1b940787377de6fd7bf6d3a3d
Simon Lee <simon@leefamily.uk.net>
2020-11-13 22:49:26 +0000

    WinPE - Inital Media Push
    Setup Structure and WinPE Configure Script

---
+++ winpe/SetupWinPEStructure.ps1
@@ -0,0 +1,30 @@
+<#
+.NAME
+    WinPE Media Builder Setup Script
+
+.DESCRIPTION
+
+.AUTHOR
+    Simon Lee
+    #HypervLAB
+    @smoon_lee
+
+.CHANGELOG
+    13.11.2020 - Inital Script Created
+
+#>
+
+# Define Setup Directory
+$SetupDir = Read-Host -Prompt 'Please Enter Setup Directory Drive Letter (Example: C:\)'
+$Folder = 'WinPERoot'
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder"
+
+# Folder Structure
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "Notes"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_Applications"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_Backgrounds"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_Drivers"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_Patches"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_ISO"
+New-Item -ItemType 'Directory' -Path "$SetupDir" + "$Folder" + \ + "WinPE_Unattend"
+
